7c0e2c98216db1a151561c18f72d89924a9c4a91
Alan Costa Facchini <costafacchini@gmail.com>
2021-03-27 11:54:51 -0300

    Define basic application structure

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter, Switch } from "react-router-dom";
+
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
