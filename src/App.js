fb6ed1281d1b86f7e756f2ec29d031a383124b64
Alan Costa Facchini <costafacchini@gmail.com>
2021-03-28 10:34:29 -0300

    Create style default for app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import './App.css';
 import Routes from "./routes";
+import { GlobalStyle } from "./styles/global";
 
 function App() {
   return (
-    <Routes />
+    <>
+      <Routes />
+      <GlobalStyle />
+    </>
   );
 }
 
